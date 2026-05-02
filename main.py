2d9a21704f875ab3f7d33087b895dd039834ba1f
YitbarekE <yitbarekendale.2024@gmail.com>
2026-05-02 19:36:50 +0300

    reverse a string algorithm

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-def main():
-    print("Hello from algorithms!")
+# def main():
+#     print("Hello from algorithms!")
 
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
+
+def some_func(s):
+  r = ""
+  for i in s[::-1]:
+    r += i
+  return r
+
+print(some_func("yeah"))
