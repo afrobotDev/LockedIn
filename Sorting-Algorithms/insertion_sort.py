d73fbe7492ba9c567f04a778da40d5adcb548ddf
YitbarekE <yitbarekendale.2024@gmail.com>
2026-05-05 15:52:27 +0300

    sort out users affliate deals by revenue

---
+++ Sorting-Algorithms/insertion_sort.py
@@ -0,0 +1,12 @@
+def insertion_sort(nums):
+    if len(nums) < 2:
+        return nums
+    for i in range(1, len(nums)):
+        key = nums[i]
+        j = i - 1
+        while j >= 0 and nums[j] > key:
+            nums[j + 1] = nums[j]
+            j -= 1  
+        nums[j + 1] = key
+ 
+    return nums
