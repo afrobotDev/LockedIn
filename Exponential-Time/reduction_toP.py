48de0fdbcb166847e4fe5f2c71dce16fd27ea276
YitbarekE <yitbarekendale.2024@gmail.com>
2026-05-07 18:58:01 +0300

    make fibonacci function to achieve polynomial runtime

---
+++ Exponential-Time/reduction_toP.py
@@ -0,0 +1,14 @@
+def fib(n):
+    if n <= 1:
+        return n
+    grandparent = 0
+    parent = 1
+    current = 0
+    for i in range(n - 1):
+        current = parent + grandparent
+        grandparent = parent
+        parent = current
+    return current
+
+
+
