2aa75c6a6fc7d7f9a711690272ce46681797479f
YitbarekE <yitbarekendale.2024@gmail.com>
2026-05-08 08:39:03 +0300

    this tricky algorithm has a Big-O of O(n)

---
+++ Sorting-Algorithms/tricky_ordern.py
@@ -0,0 +1,12 @@
+#  halvedSections returns a list of lists.
+
+def halved_sections(n):
+    rows = []
+    i = n
+    while i > 0:
+        col = []
+        for j in range(i+1):
+            col.append(j)
+        rows.append(col)
+        i //= 2
+    return rows
