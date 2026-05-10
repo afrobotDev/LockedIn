4facfee1f6ea2eb30fadb7723ecb80d090ef24c0
YitbarekE <yitbarekendale.2024@gmail.com>
2026-05-10 17:14:48 +0300

    add new nodes to head and tail of the linked list

---
+++ Linked-list/add_toHead_Tail.py
@@ -0,0 +1,36 @@
+from linked_lists import Node
+
+class LinkedList:
+    # add a new node to the head
+    def add_to_head(self, node):
+        node.next = self.head 
+        self.head = node
+
+    # add_to_tail adds a new node to the end of the list
+    def add_to_tail(self, node):
+        if not self.head:
+            self.head = node 
+            return 
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = node
+
+
+    def __init__(self):
+        self.head = None
+
+
+    def __iter__(self):
+        node = self.head
+        while node is not None:
+            yield node
+            node = node.next
+
+    def __repr__(self):
+        nodes = []
+        for node in self:
+            nodes.append(node.val)
+        return " -> ".join(nodes)
+
+
