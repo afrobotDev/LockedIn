a32a222647d608ecb3081d9fc13b39ce2ee0c793
YitbarekE <yitbarekendale.2024@gmail.com>
2026-05-11 12:23:11 +0300

    insert method takes user object and adds to the tree if i doesnt exit

---
+++ Binary-Trees/insert_nodes.py
@@ -0,0 +1,26 @@
+class BSTNode:
+
+    def __init__(self, val=None):
+        self.left = None
+        self.right = None
+        self.val = val
+
+
+    def insert(self, val):
+            if not self.val:
+                self.val = val
+                return
+            elif self.val == val:
+                return
+            elif self.val > val and not self.left:
+                self.left = BSTNode(val)
+                return
+            elif self.val > val and self.left:
+                self.left.insert(val)
+                return
+            else:
+                if self.right:
+                    self.right.insert(val)
+                    return
+                self.right = BSTNode(val)
+                return
