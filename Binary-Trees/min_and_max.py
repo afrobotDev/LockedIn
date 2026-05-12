055501ce99422b2a0fe667cd8db2bd7e464ab3c4
YitbarekE <yitbarekendale.2024@gmail.com>
2026-05-12 09:23:15 +0300

    Add get_min and get_max methods to BSTNode

---
+++ Binary-Trees/min_and_max.py
@@ -0,0 +1,39 @@
+class BSTNode:
+    def get_min(self):
+        if self.val is None:
+            return None
+        if self.left is None:
+            return self.val
+        return self.left.get_min()
+
+    def get_max(self):
+        if self.val is None:
+            return None
+        if self.right is None:
+            return self.val
+        return self.right.get_max()
+
+    def __init__(self, val=None):
+        self.left = None
+        self.right = None
+        self.val = val
+
+    def insert(self, val):
+        if not self.val:
+            self.val = val
+            return
+        if self.val == val:
+            return
+        if val < self.val:
+            if self.left:
+                self.left.insert(val)
+                return
+            self.left = BSTNode(val)
+            return 
+
+        if self.right:
+            self.right.insert(val)
+            return
+        self.right = BSTNode(val)
+
+
