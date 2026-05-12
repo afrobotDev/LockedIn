4edc7839f4dfe22012d92b0e03222b245ece64d9
YitbarekE <yitbarekendale.2024@gmail.com>
2026-05-12 15:07:31 +0300

    Add height method to BSTNode

---
+++ Binary-Trees/height_tree.py
@@ -0,0 +1,34 @@
+class BSTNode:
+    def height(self):
+        if self.val is None:
+            return 0
+        left_height = self.left.height() if self.left else 0
+        right_height = self.right.height() if self.right else 0
+        return max(left_height, right_height) + 1
+
+
+    def __init__(self, val=None):
+        self.left = None
+        self.right = None
+        self.val = val
+
+    def insert(self, val):
+        if not self.val:
+            self.val = val
+            return
+
+        if self.val == val:
+            return
+
+        if val < self.val:
+            if self.left:
+                self.left.insert(val)
+                return
+            self.left = BSTNode(val)
+            return
+
+        if self.right:
+            self.right.insert(val)
+            return
+        self.right = BSTNode(val)
+
