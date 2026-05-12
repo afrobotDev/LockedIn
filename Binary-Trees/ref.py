63911a7b9a5c687d93e590ab12f560d1f879b609
YitbarekE <yitbarekendale.2024@gmail.com>
2026-05-12 09:10:12 +0300

    Simplify ref.py to only include ref_inorder function

--- Binary-Trees/ref.py
+++ Binary-Trees/ref.py
@@ -1,32 +1,8 @@
-class BSTNode:
-    def __init__(self, val=None):
-        self.left = None
-        self.right = None
-        self.val = val
-
-    def insert(self, val):
-        if not self.val:
-            self.val = val
-            return
-        elif self.val == val:
-            return
-        elif self.val > val:
-            if not self.left:
-                self.left = BSTNode(val)
-            else:
-                self.left.insert(val)
-        else:
-            if not self.right:
-                self.right = BSTNode(val)
-            else:
-                self.right.insert(val)
-
-def ref_implementation(bst, val):
-    bst.insert(val)
-
-def ref_inorder(bst, lst):
-    if bst:
-        ref_inorder(bst.left, lst)
-        lst.append(bst.val)
-        ref_inorder(bst.right, lst)
-    return lst
+def ref_inorder(node, result):
+    if node is None:
+        return result
+    ref_inorder(node.left, result)
+    if node.val is not None:
+        result.append(node.val)
+    ref_inorder(node.right, result)
+    return result
